15b98ca3cb77f48ad4a3b9e2dca84e1515a6f42e
PlayLucky <timo.koesters@gmail.com>
2017-11-12 19:27:47 +0100

    Added zsh promptline

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-rm $HOME/.zshrc $HOME/.tmux.conf $HOME/.emacs $HOME/.emacs.d/init.el $HOME/.config/sway/config $HOME/.config/nvim/init.vim $HOME/.local/share/konsole/gruvbox.colorscheme $HOME/.tmux/tmuxline
+rm $HOME/.zshrc $HOME/.tmux.conf $HOME/.emacs $HOME/.emacs.d/init.el $HOME/.config/sway/config $HOME/.config/nvim/init.vim $HOME/.local/share/konsole/gruvbox.colorscheme $HOME/.tmux/tmuxline $HOME/.zsh/promptline
 ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
 ln -s $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
 ln -s $HOME/dotfiles/.emacs $HOME/.emacs
@@ -9,3 +9,4 @@ ln -s $HOME/dotfiles/.config/sway/config $HOME/.config/sway/config
 ln -s $HOME/dotfiles/.config/nvim/init.vim $HOME/.config/nvim/init.vim
 ln -s $HOME/dotfiles/.local/share/konsole/gruvbox.colorscheme $HOME/.local/share/konsole/gruvbox.colorscheme
 ln -s $HOME/dotfiles/.tmux/tmuxline $HOME/.tmux/tmuxline
+ln -s $HOME/dotfiles/.zsh/promptline $HOME/.zsh/promptline
