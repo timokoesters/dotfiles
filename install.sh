648a923a0bec58eeb41de4d0e02ef3d4082a4541
timokoesters <timokoesters@protonmail.com>
2018-09-05 18:35:38 +0200

    Cleaned up installer

--- install.sh
+++ install.sh
@@ -1,19 +1,15 @@
 #!/bin/sh
 
-ln -sf $HOME/dotfiles/.zshrc $HOME/.zshrc
 ln -sf $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
-ln -sf $HOME/dotfiles/.emacs $HOME/.emacs
-ln -sf $HOME/dotfiles/.emacs.d/init.el $HOME/.emacs.d/init.el
 ln -sf $HOME/dotfiles/.config/sway/config $HOME/.config/sway/config
+ln -sf $HOME/dotfiles/.config/i3status/config $HOME/.config/i3status/config
 ln -sf $HOME/dotfiles/.config/nvim/init.vim $HOME/.config/nvim/init.vim
-ln -sf $HOME/dotfiles/.local/share/konsole/gruvbox.colorscheme $HOME/.local/share/konsole/gruvbox.colorscheme
-ln -sf $HOME/dotfiles/.tmux/tmuxline $HOME/.tmux/tmuxline
-ln -sf $HOME/dotfiles/.zsh/promptline $HOME/.zsh/promptline
-ln -sf $HOME/dotfiles/.config/ranger/bookmarks $HOME/.config/ranger/bookmarks
-ln -sf $HOME/dotfiles/.config/ranger/commands $HOME/.config/ranger/commands
 ln -sf $HOME/dotfiles/.config/ranger/rc.conf $HOME/.config/ranger/rc.conf
 ln -sf $HOME/dotfiles/.config/ranger/scope.sh $HOME/.config/ranger/scope.sh
-ln -sf $HOME/dotfiles/.config/i3status/config $HOME/.config/i3status/config
 ln -sf $HOME/dotfiles/.config/termite/config $HOME/.config/termite/config
-ln -sf $HOME/dotfiles/.zprofile $HOME/.zprofile
+ln -sf $HOME/dotfiles/.local/share/konsole/gruvbox.colorscheme $HOME/.local/share/konsole/gruvbox.colorscheme
+ln -sf $HOME/dotfiles/.zshrc $HOME/.zshrc
+ln -sf $HOME/dotfiles/.zsh/promptline $HOME/.zsh/promptline
 ln -sf $HOME/dotfiles/.zshenv $HOME/.zshenv
+ln -sf $HOME/dotfiles/.zprofile $HOME/.zprofile
+ln -sf $HOME/dotfiles/.tmux/tmuxline $HOME/.tmux/tmuxline
